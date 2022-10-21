0c4ef9d43d5455cc602173f68a9dd42b06b2108d
Jon Schneider <jon.schneider@appfolio.com>
2022-10-21 07:57:05 -0400

    Add tool to quote list items

--- src/Tools/Lists.js
+++ src/Tools/Lists.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
 export default class Lists extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quotationMark: "'"
+    };
+    this.handleQuotationMarkChange = this.handleQuotationMarkChange.bind(this);
+  }
 
   // Adapted from http://stackoverflow.com/a/1144788/12484
   escapeRegExp(str) {
@@ -27,15 +34,42 @@ export default class Lists extends React.Component {
     this.props.setText(sortedCsv);
   }
 
+  quoteListItems() {
+    const lines = this.props.text.split(/\r?\n/);
+    const quotationMark = this.state.quotationMark;
+    lines.forEach(function (line, index, lines) {
+      line = line.trim();
+      if (line !== '') { 
+        line = line.replaceAll(',', (quotationMark + ',' + quotationMark));
+        line = quotationMark + line + quotationMark;
+        lines[index] = line;
+      }
+    });
+    const result = lines.join('\n');
+    this.props.setText(result);
+  }
+
+  handleQuotationMarkChange(event) {
+    this.setState({quotationMark: event.target.value});  
+  }
+
   render() {
     return (
       <fieldset className="well well-sm">
         <legend>Lists</legend>
         <p>
-          Convert whitespace-separated string to comma-separated
+          Convert whitespace-separated to comma-separated
           <input type="button" className="btn btn-primary" onClick={() => this.replaceWhitespaceWithComma()} value="Convert" />
         </p>
-        Sort comma-separated <input type="button" className="btn btn-primary" onClick={() => this.sortCsv()} value="Sort" />
+        <p>
+          Sort comma-separated <input type="button" className="btn btn-primary" onClick={() => this.sortCsv()} value="Sort" />
+        </p>
+        Surround each item with{' '}
+        <select value={this.state.quotationMark} onChange={this.handleQuotationMarkChange}>
+          <option value="'">'</option>
+          <option value='"'>"</option>
+        </select>{' '}
+        <input type="button" className="btn btn-primary" onClick={() => this.quoteListItems()} value="Quote" />
        </fieldset>
     );
   }
